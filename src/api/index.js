b179b664377c2692963a4bea6e74f3f999d7c7eb
CH <chenghao@corp-ci.com>
2017-11-29 22:56:19 +0800

    init

--- src/api/index.js
+++
@@ -1,5 +0,0 @@
-import * as api from './login';
-import * as api from './user';
-import * as api from './forum';
-
-export default {login, user, forum };
--- src/main.js
+++ src/main.js
@@ -1,51 +1,15 @@
-import babelpolyfill from 'babel-polyfill'
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-default/index.css'
-import VueRouter from 'vue-router'
-import store from './vuex/store'
-import Vuex from 'vuex'
-import NProgress from 'nprogress'
-import 'nprogress/nprogress.css'
-import routes from './router'
-import 'font-awesome/css/font-awesome.min.css'
-import Qs from 'qs'
+import router from './router'
 
-Vue.use(ElementUI)
-Vue.use(VueRouter)
-Vue.use(Vuex)
-Vue.use(Qs)
+Vue.config.productionTip = false
 
-NProgress.configure({ showSpinner: false });
-
-const router = new VueRouter({
-  routes
-})
-
-router.beforeEach((to, from, next) => {
-  //NProgress.start();
-  if (to.path == '/login') {
-    sessionStorage.removeItem('user');
-  }
-  let user = JSON.parse(sessionStorage.getItem('user'));
-  if (!user && to.path != '/login') {
-    next({ path: '/login' })
-  } else {
-    next()
-  }
-})
-
-//router.afterEach(transition => {
-//NProgress.done();
-//});
-
+/* eslint-disable no-new */
 new Vue({
-  //el: '#app',
-  //template: '<App/>',
+  el: '#app',
   router,
-  store,
-  //components: { App }
-  render: h => h(App)
-}).$mount('#app')
-
+  template: '<App/>',
+  components: { App }
+})
